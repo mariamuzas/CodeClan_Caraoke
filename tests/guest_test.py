d662044a742af7714683bbf89530b07ac6fe633a
Your Name <mmuzasantuna@gmail.com>
2020-09-20 12:03:55 +0100

    gues_name and gues_wallet tests passed

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,5 +5,16 @@ from classes.guest import Guest
 from classes.song import Song
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest1 = Guest("Pepe", 50, 100)
+
+    def test_guest_name(self):
+        self.assertEqual("Pepe", self.guest1.name)
+    
+    def test_guest_wallet(self):
+        self.assertEqual(100, self.guest1.wallet)
+    
+    
+    
+
         
