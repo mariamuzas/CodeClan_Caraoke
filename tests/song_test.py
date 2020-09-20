73093a58df5831bf36c579d323aab54616dd9899
Your Name <mmuzasantuna@gmail.com>
2020-09-20 12:07:09 +0100

    song_name test passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -5,4 +5,6 @@ from classes.guest import Guest
 from classes.song import Song
 
 class TestSong(unittest.TestCase):
-   pass
+   def test_guest_name(self):
+        self.song_1 = Song("Under Pressure", "Queen")
+        self.assertEqual("Under Pressure", self.song_1.title)
